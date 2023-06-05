34e6bf580c4631f4e83f72274ba2244ff270ae6a
Marcel Eisentraut <meisentraut@intershop.de>
2023-06-05 11:25:51 +0200

    feat: implement caching functionality to avoid unnecessary REST requests

--- src/app/extensions/address-doctor/facades/address-doctor.facade.ts
+++ src/app/extensions/address-doctor/facades/address-doctor.facade.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { isEqual } from 'lodash-es';
+import { Observable, of, tap } from 'rxjs';
 
 import { Address } from 'ish-core/models/address/address.model';
 
@@ -9,7 +10,19 @@ import { AddressDoctorApiService } from '../services/address-doctor-api/address-
 export class AddressDoctorFacade {
   private addressDoctorApi = inject(AddressDoctorApiService);
 
+  private lastAddressCheck: Address;
+  private lastAddressCheckResult: Address[] = [];
+
   checkAddress(address: Address): Observable<Address[]> {
-    return this.addressDoctorApi.postAddress(address);
+    if (isEqual(address, this.lastAddressCheck)) {
+      return of(this.lastAddressCheckResult);
+    }
+
+    this.lastAddressCheck = address;
+    return this.addressDoctorApi.postAddress(address).pipe(
+      tap(result => {
+        this.lastAddressCheckResult = result;
+      })
+    );
   }
 }
