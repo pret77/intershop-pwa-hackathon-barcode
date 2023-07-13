3432ea2e68f7b635e5d7be6bf97c0f5d06f671ea
Silke <s.grueber@intershop.de>
2023-07-13 16:38:48 +0200

    refactor: adapt imports for makeStateKey and TransferState

--- src/app/shell/application/cookies-banner/cookies-banner.component.ts
+++ src/app/shell/application/cookies-banner/cookies-banner.component.ts
@@ -1,6 +1,5 @@
 import { AnimationEvent } from '@angular/animations';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { TransferState } from '@angular/platform-browser';
+import { ChangeDetectionStrategy, Component, OnInit, TransferState } from '@angular/core';
 
 import bottomOutAnimation from 'ish-core/animations/bottom-out.animation';
 import { COOKIE_CONSENT_VERSION } from 'ish-core/configurations/state-keys';
@@ -20,7 +19,10 @@ export class CookiesBannerComponent implements OnInit {
   showBanner = false;
   transitionBanner: string = undefined;
 
-  constructor(private transferState: TransferState, private cookiesService: CookiesService) {}
+  constructor(
+    private transferState: TransferState,
+    private cookiesService: CookiesService
+  ) {}
 
   ngOnInit() {
     this.showBannerIfNecessary();
