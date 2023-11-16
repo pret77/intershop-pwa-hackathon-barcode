aa76049e3e6493c5973a29872475a2fd5fef331f
Elias H <elias.hulscher@fenego.be>
2023-11-16 10:53:28 +0100

    fix scanner 1

--- src/app/hacketon-scanner/hacketon-scanner.module.ts
+++ src/app/hacketon-scanner/hacketon-scanner.module.ts
@@ -4,10 +4,11 @@ import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 import { HacketonScanner1Component } from './scanner1/hacketon-scanner1.component';
 import { HacketonScanner2Component } from './scanner2/hacketon-scanner2.component';
+import { HacketonScannersComponent } from './scanners/hacketon-scanners.component';
 
 @NgModule({
-  declarations: [HacketonScanner1Component, HacketonScanner2Component],
+  declarations: [HacketonScanner1Component, HacketonScanner2Component, HacketonScannersComponent],
   imports: [CommonModule, ZXingScannerModule],
-  exports: [HacketonScanner1Component, HacketonScanner2Component],
+  exports: [HacketonScannersComponent],
 })
 export class HacketonScannerModule {}
---
+++ src/app/hacketon-scanner/scanners/hacketon-scanners.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ish-hacketon-scanners',
+  templateUrl: './hacketon-scanners.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HacketonScannersComponent {}
